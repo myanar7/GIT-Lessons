2678c8454f1379cd3e85eb397bec2da2ca95c54c
Mustafa Yanar <mustafayanar0007@gmail.com>
2021-03-02 16:12:40 +0300

    Update SystemProgramming.cmd

--- Lessons/SystemProgramming.cmd
+++ Lessons/SystemProgramming.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 for /F "tokens=1" %%i in ('date /t') do set mydate=%%i
-IF %mydate%==Pzt (start Chrome https://zoom.us/w/94008873902?tk=I24tdusXT0qClrnzCX4Rda78mSIiQV8BFuZuN980uzM.DQIAAAAV412TrhZsUm40bDBNaFE0NjNpd3pNYzY0SHlRAAAAAAAAAAAAAAAAAAAAAAAAAAAA&pwd=Zml6ajczY1Z3UnEyUEtYRC9nWWRyZz09) ELSE IF %mydate%==Sal (start Chrome https://zoom.us/w/94008873902?tk=I24tdusXT0qClrnzCX4Rda78mSIiQV8BFuZuN980uzM.DQIAAAAV412TrhZsUm40bDBNaFE0NjNpd3pNYzY0SHlRAAAAAAAAAAAAAAAAAAAAAAAAAAAA&pwd=Zml6ajczY1Z3UnEyUEtYRC9nWWRyZz09) ELSE ( msg %username% Bugun Ders Yok)
+IF %mydate%==Pzt (start Chrome YOUR_SYSTEM_LINK) ELSE ( msg %username% Bugun Ders Yok)
