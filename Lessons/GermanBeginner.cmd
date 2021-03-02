794006dd255634218452aa8b084baa974a25ca39
Mustafa Yanar <mustafayanar0007@gmail.com>
2021-03-02 16:09:47 +0300

    Update GermanBeginner.cmd

--- Lessons/GermanBeginner.cmd
+++ Lessons/GermanBeginner.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 for /F "tokens=1" %%i in ('date /t') do set mydate=%%i
-IF %mydate%==Sal (start Chrome https://ues.marmara.edu.tr/Activity/Index/B092E2D1A7F6C88A11AA64549D91FAF5) ELSE ( msg %username% Bugun Ders Yok)
+IF %mydate%==Sal (start Chrome YOUR_GERMAN_LINK) ELSE ( msg %username% Bugun Ders Yok)
